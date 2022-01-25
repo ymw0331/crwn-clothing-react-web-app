129388b23878080e2064df6595e6f438fe69aa54
Wayne Yong <ymw0331@gmail.com>
2022-01-25 11:29:04 +0800

    update our menu-items to navigate withRoter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,19 +12,19 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
           id: 1,
-          linkUrl: "shop/hats",
+          linkUrl: "hats",
         },
         {
           title: "jackets",
           imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
           id: 2,
-          linkUrl: "shop/jackets",
+          linkUrl: "",
         },
         {
           title: "sneakers",
           imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
           id: 3,
-          linkUrl: "shop/sneakers",
+          linkUrl: "",
         },
         {
           title: "womens",
@@ -32,7 +32,7 @@ class Directory extends React.Component {
           size: "large",
           id: 4,
           size: "large",
-          linkUrl: "shop/womens",
+          linkUrl: "",
         },
         {
           title: "mens",
@@ -40,7 +40,7 @@ class Directory extends React.Component {
           size: "large",
           id: 5,
           size: "large",
-          linkUrl: "shop/mens",
+          linkUrl: "",
         },
       ],
     };
@@ -49,8 +49,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
